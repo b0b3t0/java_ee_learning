db3b5ab85aa61b55cec650af0fd8ef3953655e13
Bojidar Nenov <cruelbobo@abv.bg>
2020-04-22 15:55:00 +0300

    Added '/' in cars-all servlet url path to fix build error

--- src/main/java/web/CarsAllServlet.java
+++ src/main/java/web/CarsAllServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("cars/all")
+@WebServlet("/cars/all")
 public class CarsAllServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
