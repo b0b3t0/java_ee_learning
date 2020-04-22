4106e0aecf4f648f8b401e2041077cb52a717303
Bojidar Nenov <cruelbobo@abv.bg>
2020-04-22 15:46:47 +0300

    Created carsCreate and carsAll servlets

--- src/main/java/web/CarsAllServlet.java
+++ src/main/java/web/CarsAllServlet.java
@@ -7,11 +7,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/cars/create")
-public class CarsCreateServlet extends HttpServlet {
+@WebServlet("cars/all")
+public class CarsAllServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/car-create.jsp")
+        req.getRequestDispatcher("/cars-all.jsp")
                 .forward(req, resp);
     }
 }
