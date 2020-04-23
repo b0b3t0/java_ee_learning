1cba986d15767c56c63152495c85c49b72ae7044
Bojidar Nenov <cruelbobo@abv.bg>
2020-04-23 11:11:59 +0300

    Created car and user entities and made relationship between them

--- src/main/java/model/entity/Car.java
+++ src/main/java/model/entity/Car.java
@@ -1,4 +1,65 @@
 package model.entity;
 
-public class Car {
+import org.hibernate.annotations.JoinColumnOrFormula;
+
+import javax.persistence.*;
+
+@Entity
+public class Car extends BaseEntity {
+    private String brand;
+    private String model;
+    private String year;
+    private Engines engines;
+    private User user;
+
+    public Car() {
+
+    }
+
+    @Column(nullable = false)
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    @Column(nullable = false)
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    @Column(nullable = false)
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    public Engines getEngines() {
+        return engines;
+    }
+
+    public void setEngines(Engines engines) {
+        this.engines = engines;
+    }
+
+    @ManyToOne
+    @JoinColumn(referencedColumnName = "id", name = "user_id")
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
