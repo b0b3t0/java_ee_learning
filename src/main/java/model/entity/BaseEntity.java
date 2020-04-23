4ce2cdbbebfe4772e2af38ae50327ba76d102de7
Bojidar Nenov <cruelbobo@abv.bg>
2020-04-23 11:13:21 +0300

    Created base entity marked with annotation MappedSupperClass to hold id field for car and user

--- src/main/java/model/entity/BaseEntity.java
+++ src/main/java/model/entity/BaseEntity.java
@@ -1,4 +1,25 @@
 package model.entity;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class BaseEntity {
+    private Integer id;
+
+    protected BaseEntity() {
+
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
